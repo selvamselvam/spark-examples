c210d8b2a65aff5957621d97e5b1262c3eef3ca6
Selvam Ramasamy <selvamselvam@hotmail.com>
2021-05-14 20:25:38 +0530

    Add files via upload

--- python/hello-world.py
+++ python/hello-world.py
@@ -1,13 +1,13 @@
 """SimpleApp.py"""
 from pyspark.sql import SparkSession
 
-logFile = "C:/software/spark/spark-3.1.1-bin-hadoop3.2/README.md"  # Should be some file on your system
+logFile = "../data/example.text"  # Should be some file on your system
 spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
 logData = spark.read.text(logFile).cache()
 
-numAs = logData.filter(logData.value.contains('a')).count()
-numBs = logData.filter(logData.value.contains('b')).count()
+numAs = logData.filter(logData.value.contains('e')).count()
+numBs = logData.filter(logData.value.contains('t')).count()
 
-print("Lines with a: %i, lines with b: %i" % (numAs, numBs))
+print("Lines with e: %i, lines with t: %i" % (numAs, numBs))
 
 spark.stop()
